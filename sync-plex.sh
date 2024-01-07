53109199b3a0af1d30ec5ee710c678ed21e1e637
Matt Manuel <matt-the-ogre@users.noreply.github.com>
2024-01-07 13:17:20 -0800

    Create sync-plex.sh

---
+++ sync-plex.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define source and destination folders
+source_folder="$HOME/Plex"
+destination_folder="$HOME/Library/CloudStorage/SynologyDrive-mattnoapple/Plex"
+
+# Perform two-way synchronization without deletions
+rsync -avh --update "$source_folder/" "$destination_folder/"
+rsync -avh --update "$destination_folder/" "$source_folder/"
+
+echo "Two-way sync completed without deletions!"
